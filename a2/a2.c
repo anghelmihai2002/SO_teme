acf0e0a6baa586e4d453ace94e5436d2c58caf8b
anghelmihai2002 <Anghel.Ma.Mihai@student.utcluj.ro>
2023-05-06 00:37:50 +0300

    incercarea 2

--- a2/a2.c
+++ a2/a2.c
@@ -1,21 +1,35 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "a2_helper.h"
 
+sem_t logSem3, logSem5;
+
+
 void *thread_function_9(void *unused)
 {
     int *j = (int*) unused;
+    if(*j == 3){
+        info(BEGIN, 9, *j);
+        sem_post(&logSem3);
+        sem_wait(&logSem5);
+        info(END, 9, *j);
+        return NULL;
+    }
+    if(*j == 5){
+        sem_wait(&logSem3);
+        info(BEGIN, 9, *j);
+        info(END, 9, *j);
+        sem_post(&logSem5);
+        return NULL;
+    }
     info(BEGIN, 9, *j);
-    if(*j != 3 && *j != 5){
-        info(END, 9, *j);
-    }
-    else if(*j == 5){
-        info(END, 9, *j);
-        info(END, 9, *j - 2);
-    }
+    info(END, 9, *j);
+    free(j);
     return NULL;
 }
 
@@ -24,6 +38,7 @@ void *thread_function_6(void *unused)
     int *j = (int*) unused;
     info(BEGIN, 6, *j);
     info(END, 6, *j);
+    free(j);
     return NULL;
 }
 
@@ -31,13 +46,8 @@ void *thread_function_8(void *unused)
 {
     int *j = (int*) unused;
     info(BEGIN, 8, *j);
-    if(*j < 10 || *j > 14){
-        info(END, 8, *j);
-    }
-    else if(*j == 14){
-        for(int i = 14; i > 9; i--)
-            info(END, 8, i);
-    }
+    info(END, 8, *j);
+    free(j);
     return NULL;
 }
 
@@ -69,11 +79,17 @@ int main(){
             pid8 = fork();
             if(pid8 == 0){
                 info(BEGIN, 8, 0);
+                sem_init(&logSem3, 0, 0);
                 pthread_t tids[37];
-                for(int i = 1; i <= 36; i++){
-                    pthread_create(&tids[i], NULL, thread_function_8, &i);
+                for(int i = 1; i < 37; i++){
+                    int *k = (int*)malloc(sizeof(int));
+                    *k = i;
+                    pthread_create(&tids[i], NULL, thread_function_8, k);
+                }
+                for(int i = 1; i < 37; i++){
                     pthread_join(tids[i], NULL);
                 }
+                sem_destroy(&logSem3);
                 info(END, 8, 0);
                 return 0;
             }
@@ -81,11 +97,19 @@ int main(){
             pid9 = fork();
             if(pid9 == 0){
                 info(BEGIN, 9, 0);
+                sem_init(&logSem3, 0, 0);
+                sem_init(&logSem5, 0, 0);
                 pthread_t tids[6];
                 for(int i = 1; i < 6; i++){
-                    pthread_create(&tids[i], NULL, thread_function_9, &i);
+                    int *k = (int*)malloc(sizeof(int));
+                    *k = i;
+                    pthread_create(&tids[i], NULL, thread_function_9, k);
+                }
+                for(int i = 1; i < 6; i++){
                     pthread_join(tids[i], NULL);
                 }
+                sem_destroy(&logSem3);
+                sem_destroy(&logSem5);
                 info(END, 9, 0);
                 return 0;
             }
@@ -97,11 +121,17 @@ int main(){
         pid6 = fork();
         if(pid6 == 0){
             info(BEGIN, 6, 0);
+            sem_init(&logSem3, 0, 0);
             pthread_t tids[7];
             for(int i = 1; i < 7; i++){
-                pthread_create(&tids[i], NULL, thread_function_6, &i);
+                int *k = (int*)malloc(sizeof(int));
+                *k = i;
+                pthread_create(&tids[i], NULL, thread_function_6, k);
+            }
+            for(int i = 1; i < 7; i++){
                 pthread_join(tids[i], NULL);
             }
+            sem_destroy(&logSem3);
             info(END, 6, 0);
             return 0;
         }
